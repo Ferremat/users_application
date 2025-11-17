b493f1e714f1a895d9c78907a535dbda5422550b
Vixtinity <08062006ismael@gmail.com>
2025-11-17 13:17:20 +0100

    imports fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
-import { UsersController } from './modules/users/users.controller';
-import { UsersService } from './modules/users/users.service';
-import { LoginModule } from './modules/users/submodules/login/login.module';
-import { LoginController } from './modules/users/submodules/login/login.controller';
-import { LoginService } from './modules/users/submodules/login/login.service';
-import { ProfileModule } from './modules/users/submodules/profile/profile.module';
-import { ProfileController } from './modules/users/submodules/profile/profile.controller';
-import { ProfileService } from './modules/users/submodules/profile/profile.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [UsersModule, LoginModule, ProfileModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT !== undefined ? parseInt(process.env.DB_PORT, 10) : undefined,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: 'users_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, UsersService, LoginService, ProfileService],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {}
+}
